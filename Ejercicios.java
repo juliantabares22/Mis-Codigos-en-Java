8c2ac9f119bcc0b04b144716fc6fa6371c95fef5
juliantabares22@gmail.com <juliantabares22@gmail.com>
2021-09-23 20:08:56 -0500

    Ejercicios nueva carpeta

---
+++ Ejercicios.java
@@ -0,0 +1,39 @@
+
+public class Ejercicios {
+
+    public static void main(String args[]) {
+        /*System.out.println("Hola Mundo En Java");
+        System.out.println("Julián Tabares");
+        
+        byte entero;
+        
+        entero = 12;
+        System.out.println("El numero entero es:" + entero);
+        
+        double decimal =3.45;
+        System.out.println("double = " + decimal);
+        
+        char caracter = '9';
+        System.out.println("caracter = " + caracter);
+        
+        boolean decision = false;
+        System.out.println("decision = " + decision);
+        
+        */
+        
+        //Integer numero = null;
+        //System.out.println("El numero es:" + numero);
+        
+        String palabra = "Hola que tal?";
+        System.out.println(palabra);
+        
+        final int numero = 10; //final es igual a constante 
+       
+        System.out.println(numero);
+        
+        
+        
+
+                
+    }
+}
